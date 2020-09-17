46815417e421b2019ae04f405ac6df0e6590f262
Fredrik Lind <fredrik.lind.96@gmail.com>
2020-09-17 17:04:08 +0200

    Added usage tips

--- main.cc
+++ main.cc
@@ -7,6 +7,21 @@
 
 #define degtorad(angle) angle * MPI / 180
 
+void print_usage()
+{
+	printf("This utility renders a single frame of a raytraced scene, "
+			"displaying it in an OpenGL window and optionally saving the finished result to a file.\n\n"
+			"usage: \ttrayracer [-h] [-w <w x h>] [-o <path>] [-r <rays>]\n"
+		       "\t[-n <name>] [-x <seed>] [-b <bounces>]\n\n"
+		       "options:\n"
+		       "\t-w <w x h>: Specify the resolution of the render.\n"
+		       "\t-o <path>: Specify an (optional)  output path for the finished image.\n"
+		       "\t-r <rays>: Select the number of rays to fire per pixel in the image (default = 1).\n"
+		       "\t-n <name>: Specify the OpenGL window name.\n"
+		       "\t-x <seed>: An optional seed for placing the spheres in the scene.\n"
+		       "\t-b <bounces>: The number of bounces to perform per ray. Higher gives improved reflections.\n\n"
+	      );
+}
 
 int main(int argc, char* argv[])
 {
@@ -27,10 +42,13 @@ int main(int argc, char* argv[])
 
 	opterr = 0;
 
-	while ((c = getopt (argc, argv, "w:o:r:n:x:b:")) != -1)
+	while ((c = getopt (argc, argv, "hw:o:r:n:x:b:")) != -1)
 	{
 		switch (c)
 		{
+			case 'h':
+				print_usage();
+				return 0;
 			case 'w':
 				if (sscanf(optarg, "%ux%u", &width, &height) != 2)
 				{
@@ -73,7 +91,7 @@ int main(int argc, char* argv[])
 	if (optind == argc - 1)
 		spheres = atoi(argv[optind]);
 
-	printf("Tracing %i spheres to buffer size %ux%u with %i rays, %i bounces.\n", spheres, width, height, rays, bounces);
+	printf("Tracing %i spheres to buffer of size %ux%u with %i rays, %i bounces.\n", spheres, width, height, rays, bounces);
 
 	Display::Window wnd;
 	
@@ -137,120 +155,38 @@ int main(int argc, char* argv[])
 	vec3 camPos = { 0,1.0f,10.0f };
 	vec3 moveDir = { 0,0,0 };
 
-	wnd.SetKeyPressFunction([&exit, &moveDir, &resetFramebuffer](int key, int scancode, int action, int mods)
+	wnd.SetKeyPressFunction([&exit](int key, int scancode, int action, int mods)
 	{
 		switch (key)
 		{
 		case GLFW_KEY_ESCAPE:
 			exit = true;
 			break;
-		case GLFW_KEY_W:
-			moveDir.z -= 1.0f;
-			resetFramebuffer |= true;
-			break;
-		case GLFW_KEY_S:
-			moveDir.z += 1.0f;
-			resetFramebuffer |= true;
-			break;
-		case GLFW_KEY_A:
-			moveDir.x -= 1.0f;
-			resetFramebuffer |= true;
-			break;
-		case GLFW_KEY_D:
-			moveDir.x += 1.0f;
-			resetFramebuffer |= true;
-			break;
-		case GLFW_KEY_SPACE:
-			moveDir.y += 1.0f;
-			resetFramebuffer |= true;
-			break;
-		case GLFW_KEY_LEFT_CONTROL:
-			moveDir.y -= 1.0f;
-			resetFramebuffer |= true;
-			break;
 		default:
 			break;
 		}
 	});
 
-	float pitch = 0;
-	float yaw = 0;
-	float oldx = 0;
-	float oldy = 0;
-
-	wnd.SetMouseMoveFunction([&pitch, &yaw, &oldx, &oldy, &resetFramebuffer](double x, double y)
-	{
-		x *= -0.1;
-		y *= -0.1;
-		yaw = x - oldx;
-		pitch = y - oldy;
-		resetFramebuffer |= true;
-		oldx = x;
-		oldy = y;
-	});
-
-	float rotx = 0;
-	float roty = 0;
-
 	// number of accumulated frames
 	int frameIndex = 0;
 
-	std::vector<Color> framebufferCopy(width * height);
+	mat4 cameraTransform;
+	cameraTransform.m30 = camPos.x;
+	cameraTransform.m31 = camPos.y;
+	cameraTransform.m32 = camPos.z;
+
+	rt.SetViewMatrix(cameraTransform);
+
+	printf("Starting trace...\n");
+	rt.Raytrace();
+	printf("Trace complete in X seconds.\n");
 
 	// rendering loop
 	while (wnd.IsOpen() && !exit)
 	{
-		resetFramebuffer = false;
-		moveDir = {0,0,0};
-		pitch = 0;
-		yaw = 0;
-
 		// poll input
 		wnd.Update();
-
-		rotx -= pitch;
-		roty -= yaw;
-
-		moveDir = normalize(moveDir);
-
-		mat4 xMat = (rotationx(rotx));
-		mat4 yMat = (rotationy(roty));
-		mat4 cameraTransform = multiply(yMat, xMat);
-
-		camPos = camPos + transform(moveDir * 0.2f, cameraTransform);
-		
-		cameraTransform.m30 = camPos.x;
-		cameraTransform.m31 = camPos.y;
-		cameraTransform.m32 = camPos.z;
-
-		rt.SetViewMatrix(cameraTransform);
-		
-		if (resetFramebuffer)
-		{
-			rt.Clear();
-			frameIndex = 0;
-		}
-
-		rt.Raytrace();
-		frameIndex++;
-
-		// Get the average distribution of all samples
-		{
-			size_t p = 0;
-			for (Color const& pixel : framebuffer)
-			{
-				framebufferCopy[p] = pixel;
-				framebufferCopy[p].r /= frameIndex;
-				framebufferCopy[p].g /= frameIndex;
-				framebufferCopy[p].b /= frameIndex;
-				p++;
-			}
-		}
-
-		glClearColor(0, 0, 0, 1.0);
-		glClear( GL_COLOR_BUFFER_BIT );
-
-		wnd.Blit((float*)&framebufferCopy[0], width, height);
+		wnd.Blit((float*)&framebuffer[0], width, height);
 		wnd.SwapBuffers();
 	}
 
