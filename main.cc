b4f8ac2e61640b4e5d8b29903b4e47e5ead93baf
Fredrik Lind <fredrik.lind.96@gmail.com>
2020-09-17 16:15:33 +0200

    Added command line input

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <getopt.h>
 #include "window.h"
 #include "vec3.h"
 #include "raytracer.h"
@@ -6,197 +7,255 @@
 
 #define degtorad(angle) angle * MPI / 180
 
-int main()
-{ 
-    Display::Window wnd;
-    
-    wnd.SetTitle("TrayRacer");
-    
-    if (!wnd.Open())
-        return 1;
 
-    std::vector<Color> framebuffer;
+int main(int argc, char* argv[])
+{
+	
+	unsigned width = 256;
+		unsigned height = 256;
 
-    const unsigned w = 200;
-    const unsigned h = 100;
-    framebuffer.resize(w * h);
-    
-    int raysPerPixel = 1;
-    int maxBounces = 5;
+	int rays = 1;
+	int seed = 1337;
+	int bounces = 5;
+	int spheres = 8;	
+	
+	char* window_name = nullptr;
+	char* output_path = nullptr;
+	
+	int index;
+	int c;
 
-    Raytracer rt = Raytracer(w, h, framebuffer, raysPerPixel, maxBounces);
+	opterr = 0;
 
-    // Create some objects
-    Material* mat = new Material();
-    mat->type = "Lambertian";
-    mat->color = { 0.5,0.5,0.5 };
-    mat->roughness = 0.3;
-    Sphere* ground = new Sphere(1000, { 0,-1000, -1 }, mat);
-    rt.AddObject(ground);
-    
-    mat = new Material();
-    mat->type = "Conductor";
-    mat->color = {1,1,1};
-    mat->roughness = 0.2f;
-    Sphere* test = new Sphere(1, { 0,1,0 }, mat);
-    rt.AddObject(test);
+	while ((c = getopt (argc, argv, "w:o:r:n:x:b:")) != -1)
+	{
+		switch (c)
+		{
+			case 'w':
+				if (sscanf(optarg, "%ux%u", &width, &height) != 2)
+				{
+					fprintf(stderr, "Invalid resolution format for '-%c': use 'WxH'\n", c);
+					return 1;
+				}
+				break;
+			case 'o':
+				output_path = optarg;
+				break;
+			case 'r':
+				if ((rays = atoi(optarg)) <= 0)
+				{
+					fprintf(stderr, "Invalid ray count for '-%c': Must be positive integer.\n", c);
+					return 1;
+				}
+				break;
+			case 'n':
+				window_name = optarg;
+				break;
+			case 'x':
+				seed = atoi(optarg);
+				break;
+			case 'b':
+				bounces = atoi(optarg);
+				break;
+			case '?':
+				if (optopt == 'c')
+					fprintf (stderr, "Option '-%c' requires an argument.\n", optopt);
+				else if (isprint (optopt))
+					fprintf (stderr, "Unknown option '-%c'.\n", optopt);
+				else
+					fprintf (stderr, "Unknown option character '\\x%x'.\n",optopt);
+				return 1;
+			default:
+				abort ();
+		}
+	}
 
-    mat = new Material();
-    mat->type = "Lambertian";
-    mat->color = { 0,0.4,0.6 };
-    mat->roughness = 0.2;
-    test = new Sphere(1, { -4,1,0 }, mat);
-    rt.AddObject(test);
+	if (optind == argc - 1)
+		spheres = atoi(argv[optind]);
 
-    mat = new Material();
-    mat->type = "Dielectric";
-    mat->color = { 1,0.8,0.7 };
-    mat->roughness = 0.95;
-    mat->refractionIndex = 1.65;
-    test = new Sphere(1, { -4,1, 2 }, mat);
-    rt.AddObject(test);
+	printf("Tracing %i spheres to buffer size %ux%u with %i rays, %i bounces.\n", spheres, width, height, rays, bounces);
 
-    mat = new Material();
-    mat->type = "Lambertian";
-    mat->color = { 1,0,0.2 };
-    mat->roughness = 0.04;
-    test = new Sphere(1, { 1,1, -3 }, mat);
-    rt.AddObject(test);
+	Display::Window wnd;
+	
+	wnd.SetTitle(window_name == nullptr ? "TrayRacer" : window_name);
+	
+	if (!wnd.Open())
+		return 1;
 
-    mat = new Material();
-    mat->type = "Lambertian";
-    mat->color = { 1,1,1 };
-    mat->roughness = 0.0;
-    test = new Sphere(1, { 4,1, 0 }, mat);
-    rt.AddObject(test);
+	std::vector<Color> framebuffer(width * height);
+	
+	Raytracer rt = Raytracer(width, height, framebuffer, rays, bounces);
 
-    bool exit = false;
+	// Create some objects
+	Material* mat = new Material();
+	mat->type = "Lambertian";
+	mat->color = { 0.5,0.5,0.5 };
+	mat->roughness = 0.3;
+	Sphere* ground = new Sphere(1000, { 0,-1000, -1 }, mat);
+	rt.AddObject(ground);
+	
+	mat = new Material();
+	mat->type = "Conductor";
+	mat->color = {1,1,1};
+	mat->roughness = 0.2f;
+	Sphere* test = new Sphere(1, { 0,1,0 }, mat);
+	rt.AddObject(test);
 
-    // camera
-    bool resetFramebuffer = false;
-    vec3 camPos = { 0,1.0f,10.0f };
-    vec3 moveDir = { 0,0,0 };
+	mat = new Material();
+	mat->type = "Lambertian";
+	mat->color = { 0,0.4,0.6 };
+	mat->roughness = 0.2;
+	test = new Sphere(1, { -4,1,0 }, mat);
+	rt.AddObject(test);
 
-    wnd.SetKeyPressFunction([&exit, &moveDir, &resetFramebuffer](int key, int scancode, int action, int mods)
-    {
-        switch (key)
-        {
-        case GLFW_KEY_ESCAPE:
-            exit = true;
-            break;
-        case GLFW_KEY_W:
-            moveDir.z -= 1.0f;
-            resetFramebuffer |= true;
-            break;
-        case GLFW_KEY_S:
-            moveDir.z += 1.0f;
-            resetFramebuffer |= true;
-            break;
-        case GLFW_KEY_A:
-            moveDir.x -= 1.0f;
-            resetFramebuffer |= true;
-            break;
-        case GLFW_KEY_D:
-            moveDir.x += 1.0f;
-            resetFramebuffer |= true;
-            break;
-        case GLFW_KEY_SPACE:
-            moveDir.y += 1.0f;
-            resetFramebuffer |= true;
-            break;
-        case GLFW_KEY_LEFT_CONTROL:
-            moveDir.y -= 1.0f;
-            resetFramebuffer |= true;
-            break;
-        default:
-            break;
-        }
-    });
+	mat = new Material();
+	mat->type = "Dielectric";
+	mat->color = { 1,0.8,0.7 };
+	mat->roughness = 0.95;
+	mat->refractionIndex = 1.65;
+	test = new Sphere(1, { -4,1, 2 }, mat);
+	rt.AddObject(test);
 
-    float pitch = 0;
-    float yaw = 0;
-    float oldx = 0;
-    float oldy = 0;
+	mat = new Material();
+	mat->type = "Lambertian";
+	mat->color = { 1,0,0.2 };
+	mat->roughness = 0.04;
+	test = new Sphere(1, { 1,1, -3 }, mat);
+	rt.AddObject(test);
 
-    wnd.SetMouseMoveFunction([&pitch, &yaw, &oldx, &oldy, &resetFramebuffer](double x, double y)
-    {
-        x *= -0.1;
-        y *= -0.1;
-        yaw = x - oldx;
-        pitch = y - oldy;
-        resetFramebuffer |= true;
-        oldx = x;
-        oldy = y;
-    });
+	mat = new Material();
+	mat->type = "Lambertian";
+	mat->color = { 1,1,1 };
+	mat->roughness = 0.0;
+	test = new Sphere(1, { 4,1, 0 }, mat);
+	rt.AddObject(test);
 
-    float rotx = 0;
-    float roty = 0;
+	bool exit = false;
 
-    // number of accumulated frames
-    int frameIndex = 0;
+	// camera
+	bool resetFramebuffer = false;
+	vec3 camPos = { 0,1.0f,10.0f };
+	vec3 moveDir = { 0,0,0 };
 
-    std::vector<Color> framebufferCopy;
-    framebufferCopy.resize(w * h);
+	wnd.SetKeyPressFunction([&exit, &moveDir, &resetFramebuffer](int key, int scancode, int action, int mods)
+	{
+		switch (key)
+		{
+		case GLFW_KEY_ESCAPE:
+			exit = true;
+			break;
+		case GLFW_KEY_W:
+			moveDir.z -= 1.0f;
+			resetFramebuffer |= true;
+			break;
+		case GLFW_KEY_S:
+			moveDir.z += 1.0f;
+			resetFramebuffer |= true;
+			break;
+		case GLFW_KEY_A:
+			moveDir.x -= 1.0f;
+			resetFramebuffer |= true;
+			break;
+		case GLFW_KEY_D:
+			moveDir.x += 1.0f;
+			resetFramebuffer |= true;
+			break;
+		case GLFW_KEY_SPACE:
+			moveDir.y += 1.0f;
+			resetFramebuffer |= true;
+			break;
+		case GLFW_KEY_LEFT_CONTROL:
+			moveDir.y -= 1.0f;
+			resetFramebuffer |= true;
+			break;
+		default:
+			break;
+		}
+	});
 
-    // rendering loop
-    while (wnd.IsOpen() && !exit)
-    {
-        resetFramebuffer = false;
-        moveDir = {0,0,0};
-        pitch = 0;
-        yaw = 0;
+	float pitch = 0;
+	float yaw = 0;
+	float oldx = 0;
+	float oldy = 0;
 
-        // poll input
-        wnd.Update();
+	wnd.SetMouseMoveFunction([&pitch, &yaw, &oldx, &oldy, &resetFramebuffer](double x, double y)
+	{
+		x *= -0.1;
+		y *= -0.1;
+		yaw = x - oldx;
+		pitch = y - oldy;
+		resetFramebuffer |= true;
+		oldx = x;
+		oldy = y;
+	});
 
-        rotx -= pitch;
-        roty -= yaw;
+	float rotx = 0;
+	float roty = 0;
 
-        moveDir = normalize(moveDir);
+	// number of accumulated frames
+	int frameIndex = 0;
 
-        mat4 xMat = (rotationx(rotx));
-        mat4 yMat = (rotationy(roty));
-        mat4 cameraTransform = multiply(yMat, xMat);
+	std::vector<Color> framebufferCopy(width * height);
 
-        camPos = camPos + transform(moveDir * 0.2f, cameraTransform);
-        
-        cameraTransform.m30 = camPos.x;
-        cameraTransform.m31 = camPos.y;
-        cameraTransform.m32 = camPos.z;
+	// rendering loop
+	while (wnd.IsOpen() && !exit)
+	{
+		resetFramebuffer = false;
+		moveDir = {0,0,0};
+		pitch = 0;
+		yaw = 0;
 
-        rt.SetViewMatrix(cameraTransform);
-        
-        if (resetFramebuffer)
-        {
-            rt.Clear();
-            frameIndex = 0;
-        }
+		// poll input
+		wnd.Update();
 
-        rt.Raytrace();
-        frameIndex++;
+		rotx -= pitch;
+		roty -= yaw;
 
-        // Get the average distribution of all samples
-        {
-            size_t p = 0;
-            for (Color const& pixel : framebuffer)
-            {
-                framebufferCopy[p] = pixel;
-                framebufferCopy[p].r /= frameIndex;
-                framebufferCopy[p].g /= frameIndex;
-                framebufferCopy[p].b /= frameIndex;
-                p++;
-            }
-        }
+		moveDir = normalize(moveDir);
 
-        glClearColor(0, 0, 0, 1.0);
-        glClear( GL_COLOR_BUFFER_BIT );
+		mat4 xMat = (rotationx(rotx));
+		mat4 yMat = (rotationy(roty));
+		mat4 cameraTransform = multiply(yMat, xMat);
 
-        wnd.Blit((float*)&framebufferCopy[0], w, h);
-        wnd.SwapBuffers();
-    }
+		camPos = camPos + transform(moveDir * 0.2f, cameraTransform);
+		
+		cameraTransform.m30 = camPos.x;
+		cameraTransform.m31 = camPos.y;
+		cameraTransform.m32 = camPos.z;
 
-    if (wnd.IsOpen())
-        wnd.Close();
+		rt.SetViewMatrix(cameraTransform);
+		
+		if (resetFramebuffer)
+		{
+			rt.Clear();
+			frameIndex = 0;
+		}
 
-    return 0;
+		rt.Raytrace();
+		frameIndex++;
+
+		// Get the average distribution of all samples
+		{
+			size_t p = 0;
+			for (Color const& pixel : framebuffer)
+			{
+				framebufferCopy[p] = pixel;
+				framebufferCopy[p].r /= frameIndex;
+				framebufferCopy[p].g /= frameIndex;
+				framebufferCopy[p].b /= frameIndex;
+				p++;
+			}
+		}
+
+		glClearColor(0, 0, 0, 1.0);
+		glClear( GL_COLOR_BUFFER_BIT );
+
+		wnd.Blit((float*)&framebufferCopy[0], width, height);
+		wnd.SwapBuffers();
+	}
+
+	if (wnd.IsOpen())
+		wnd.Close();
+
+	return 0;
 } 
